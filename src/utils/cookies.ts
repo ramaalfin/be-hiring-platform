7201d25a648699ec679daa901dbfb8eafd256a4c
ramaalfin <ramaalfin7@gmail.com>
2025-10-31 08:29:18 +0700

    push

--- src/utils/cookies.ts
+++ src/utils/cookies.ts
@@ -24,18 +24,8 @@ export const getRefreshTokenCookieOptions = (): CookieOptions => ({
 
 export const setAuthCookies = ({ res, accessToken, refreshToken }: Params) => {
   // Access token: bisa dibaca middleware FE
-  res.cookie("accessToken", accessToken, {
-    httpOnly: false,
-    secure: false,
-    expires: fifteenMinutesFromNow(),
-  });
-
-  res.cookie("refreshToken", refreshToken, {
-    httpOnly: false,
-    secure: false,
-    path: "/",
-    expires: thirtyDaysFromNow(),
-  });
+  res.cookie("accessToken", accessToken, getAccessTokenCookieOptions());
+  res.cookie("refreshToken", refreshToken, getRefreshTokenCookieOptions());
 };
 
 export const clearAuthCookies = (res: Response) => {
