ee9763be0b7c162d18ac6331e37152153ef8cbae
ramaalfin <ramaalfin7@gmail.com>
2025-11-01 22:11:56 +0700

    fix: index

--- src/index.ts
+++ src/index.ts
@@ -14,15 +14,17 @@ import applicationsRoutes from "./routes/applicant.route";
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://fe-hiring-platform.vercel.app"],
-    credentials: true,
+    origin: APP_ORIGIN,
+    credentials: true, // wajib!
   })
 );
 
 app.use(cookieParser());
-app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
