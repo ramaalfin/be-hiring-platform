0c806effc45ae8693d26b6a80496d38fe75fc8af
ramaalfin <ramaalfin7@gmail.com>
2025-11-06 10:06:54 +0700

    fix auth route

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -20,7 +20,7 @@ const authRoutes = Router();
 authRoutes.post("/register", registerController);
 authRoutes.post("/login", loginController);
 authRoutes.get("/logout", logoutController);
-authRoutes.get("/refresh", refreshController);
+authRoutes.post("/refresh", refreshController);
 authRoutes.post("/email/verify", verifyEmailController);
 authRoutes.post("/password/forgot", forgotPasswordController);
 authRoutes.post("/password/reset", resetPasswordController);
